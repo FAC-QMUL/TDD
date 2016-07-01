1efde86f6fd3ffc50748f112d327730fa43d6ade
tormod17 <tormodsmith@gmail.com>
2016-07-01 15:45:20 +0100

    provide answers to getting started jasmne

--- TDD/Jasmine/jasmine-intro/src/app.js
+++ TDD/Jasmine/jasmine-intro/src/app.js
@@ -3,6 +3,7 @@ function add(x,y){
 	total = Math.floor(total);
 	return total;
 }
+
 function getAverage(array){
 	var sum = array.reduce((p,c) => {
 		return  p+c
@@ -10,12 +11,38 @@ function getAverage(array){
 	return Math.floor(sum  / array.length)
 }
 
-function last(list){
-    console.log(list);
-    var argumentKeys  = Object.keys(arguments)
-    var lastKey = argumentKeys[argumentKeys.length-1];
-    
-    
-    return list[list.length -1]  || arguments[lastKey]
+function lastCharacter(list){
+
+	if(arguments.length === 1 && typeof(arguments[0]) === 'number'){
+		var string = arguments[0].toString()
+		return  Number(string[string.length-1]);
+	}
+	var lastKey = arguments.length-1;
+    return list[list.length -1] || arguments[lastKey]
 }
 
+function addElementToEndAndFrontOfArray(element, array){
+	array.push(element)
+	array.unshift(element)
+	return array
+
+}
+
+function addAddress(obj, address){
+	obj.address = address;
+	return obj;
+}
+
+function getGivenPerson(name,people){
+	people.filter(function(person){s
+		return person.name  === name ;
+	})
+}
+
+
+var COUNT =0;
+
+function start(){
+	COUNT++;
+	return COUNT;
+}
